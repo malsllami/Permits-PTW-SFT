e78d148ccfeab008972d4f602d483d8e4ebccba8
malsllami <44532530+malsllami@users.noreply.github.com>
2026-07-25 00:44:15 +0300

    Delete src directory

--- src/components/common/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useSession } from '../../hooks/useSession.js';
-
-export default function ProtectedRoute({ children }) {
-  const { session } = useSession();
-  if (!session) return <Navigate to="/login" replace />;
-  return children;
-}
--- src/services/employeesService.js
+++
@@ -1,8 +0,0 @@
-import { callApi } from './apiClient.js';
-
-export const listEmployees = () => callApi('listEmployees', {});
-export const searchEmployees = (query) => callApi('searchEmployees', { query });
-export const createEmployee = (employee) => callApi('createEmployee', { employee });
-export const updateEmployee = (employeeId, employee) => callApi('updateEmployee', { employeeId, employee });
-export const disableEmployee = (employeeId) => callApi('disableEmployee', { employeeId });
-export const deleteEmployee = (employeeId) => callApi('deleteEmployee', { employeeId });
