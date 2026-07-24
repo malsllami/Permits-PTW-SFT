78a4e1240883c06de54530cc43538c46cbc4a46a
malsllami <44532530+malsllami@users.noreply.github.com>
2026-07-25 00:43:49 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// base يطابق اسم مستودع GitHub Pages: https://<user>.github.io/Permits-PTW-SFT/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Permits-PTW-SFT/'
-});
